a66a6fcc4a507e448f723f17fb5812e120d6c593
Nandathilaka <thilinachamikashtc@gmail.com>
2018-03-27 14:22:24 +0530

    Create University Course Enrollment System. Add Login,Students Registration,Course Enrollment System Part

---
+++ Parttime/DAL/UniversityContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Parttime.Models;
+
+namespace Parttime.DAL
+{
+    public class UniversityContext : DbContext
+    {
+        public UniversityContext() : base("UniversityContext") {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
