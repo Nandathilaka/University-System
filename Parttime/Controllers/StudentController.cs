06d06aff6f07c21cdac7b64bbed2ddc5f26c75f8
Nandathilaka <thilinachamikashtc@gmail.com>
2018-04-02 20:13:29 +0530

    Update the Delete Page of Student

--- Parttime/Controllers/StudentController.cs
+++ Parttime/Controllers/StudentController.cs
@@ -61,9 +61,9 @@ namespace Parttime.Controllers
             catch (DataException /*dex */)
             {
                 //Log the Error
-                ModelState.AddModelError("","Unable to the save. Try again, and if the problem see your system administrator.");
+                ModelState.AddModelError("", "Unable to the save. Try again, and if the problem see your system administrator.");
             }
-            
+
             return View(student);
         }
 
@@ -94,42 +94,48 @@ namespace Parttime.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             var studentToUpdate = db.Students.Find(id);
-            if (TryUpdateModel(studentToUpdate, "", new string[] {"LastName","FirstMidName","EnrollmentDate" })) ;
+            if (TryUpdateModel(studentToUpdate, "", new string[] { "LastName", "FirstMidName", "EnrollmentDate" })) ;
             {
-                try {
+                try
+                {
                     db.SaveChanges();
                     return RedirectToAction("Index");
                 }
                 catch (DataException)
                 {
                     //Log the error
-                    ModelState.AddModelError("","Unable to save change. Try again, and if the problem persists, see your system administrator.");
+                    ModelState.AddModelError("", "Unable to save change. Try again, and if the problem persists, see your system administrator.");
                 }
             }
             return View(studentToUpdate);
         }
 
 
-       /* [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FirstMidname,LastName,EnrollmentDate")] Student student)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(student).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(student);
-        }
-        */
+        /* 
+         [HttpPost]
+         [ValidateAntiForgeryToken]
+         public ActionResult Edit([Bind(Include = "ID,FirstMidname,LastName,EnrollmentDate")] Student student)
+         {
+             if (ModelState.IsValid)
+             {
+                 db.Entry(student).State = EntityState.Modified;
+                 db.SaveChanges();
+                 return RedirectToAction("Index");
+             }
+             return View(student);
+         }
+         */
 
         // GET: Student/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult Delete(int? id, bool? saveChangesError = false)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            if (saveChangesError.GetValueOrDefault())
+            {
+                ViewBag.ErrorMessage = "Delete failed. Try again, and if the problem see your system administrator";
             }
             Student student = db.Students.Find(id);
             if (student == null)
@@ -140,13 +146,24 @@ namespace Parttime.Controllers
         }
 
         // POST: Student/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        public ActionResult Delete(int id)
         {
-            Student student = db.Students.Find(id);
-            db.Students.Remove(student);
-            db.SaveChanges();
+            try
+            {
+                Student studentToDelete = new Student() {ID= id };
+                db.Entry(studentToDelete).State = EntityState.Deleted;
+                //Student student = db.Students.Find(id);
+                //db.Students.Remove(student);
+                db.SaveChanges();
+            }
+            catch (DataException)
+            {
+                //log the Error
+                return RedirectToAction("Delete", new { id = id, saveChangesError = true });
+            }
+
             return RedirectToAction("Index");
         }
 
